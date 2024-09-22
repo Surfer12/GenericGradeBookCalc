cd76e49a4c86930aa677910afb052f770a6c7b8e
Surfer12 <ryanoatsie@outlook.com>
2024-09-22 00:20:32 -0700

    Add MainHashSet demonstration to DemoApplication

---
+++ src/main/java/main/DemoApplication.java
@@ -0,0 +1,11 @@
+package main;
+
+public class DemoApplication {
+    public static void main(String[] args) {
+        // Run the Micronaut application
+        Application.main(args);
+
+        // Run the MainHashSet demonstration
+        MainHashSet.main(args);
+    }
+}
