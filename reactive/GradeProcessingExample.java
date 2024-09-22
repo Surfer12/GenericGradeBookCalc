4d22cb9c9d9ef18fef50f78f82941545f2d6e438
Surfer12 <ryanoatsie@outlook.com>
2024-09-22 04:42:02 -0700

    Update

---
+++ reactive/GradeProcessingExample.java
@@ -0,0 +1,33 @@
+ package reactive;
+
+import org.springframework.stereotype.Component;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Flux;
+
+@Component
+public class GradeProcessingExample implements CommandLineRunner {
+
+    private final StudentGradeProcessor letterProcessor;
+    private final StudentGradeProcessor passFailProcessor;
+
+    @Autowired
+    public GradeProcessingExample(GradeStrategy letterStrategy, GradeStrategy passFailStrategy) {
+        this.letterProcessor = new StudentGradeProcessor(letterStrategy);
+        this.passFailProcessor = new StudentGradeProcessor(passFailStrategy);
+    }
+
+    @Override
+    public void run(String... args) {
+        // Create a flux of student grades
+        Flux<Integer> grades = Flux.just(95, 80, 65, 50, 75);
+
+        System.out.println("Letter Grades:");
+        letterProcessor.processGrades(grades)
+            .subscribe(System.out::println);
+
+        System.out.println("\nPass/Fail Results:");
+        passFailProcessor.processGrades(grades)
+            .subscribe(System.out::println);
+    }
+}
--- reactive/MainClass.java
+++ reactive/MainClass.java
@@ -3,7 +3,7 @@ package reactive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "reactive")
 public class MainClass {
     public static void main(String[] args) {
         SpringApplication.run(MainClass.class, args);
